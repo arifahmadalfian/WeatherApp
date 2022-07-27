c332faecd20786dd96bd0abee7a488c72362481b
arif <arifahmadalfian@gmail.com>
2022-07-27 22:59:06 +0700

    location default

---
+++ app/src/main/java/com/arifahmadalfian/weatherapp/data/location/DefaultLocationTracker.kt
@@ -0,0 +1,63 @@
+package com.arifahmadalfian.weatherapp.data.location
+
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import androidx.core.content.ContextCompat
+import com.arifahmadalfian.weatherapp.domain.location.ILocationTracker
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.suspendCancellableCoroutine
+import javax.inject.Inject
+import kotlin.coroutines.resume
+
+class DefaultLocationTracker @Inject constructor(
+    private val locationClient: FusedLocationProviderClient,
+    private val application: Application
+) : ILocationTracker {
+
+    override suspend fun getCurrentLocation(): Location? {
+        val hasAccessLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val hasAccessCoarsePermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val locationManager =
+            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (!hasAccessLocationPermission || !hasAccessCoarsePermission || !isLocationEnabled) {
+            return null
+        }
+
+        return suspendCancellableCoroutine { cont ->
+            locationClient.lastLocation.apply {
+                if (isComplete) {
+                    if (isSuccessful) {
+                        cont.resume(result)
+                    } else {
+                        cont.resume(null)
+                    }
+                    return@suspendCancellableCoroutine
+                }
+                addOnSuccessListener {
+                    cont.resume(it)
+                }
+                addOnFailureListener {
+                    cont.resume(null)
+                }
+                addOnCanceledListener {
+                    cont.cancel()
+                }
+            }
+        }
+    }
+}
